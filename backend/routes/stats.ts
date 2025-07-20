77958b893ec6c48c4e8b07b4af446570dcde03b2
Ricardoc2903 <ricardodavidquevedo29@gmail.com>
2025-07-19 21:05:48 -0300

    subiendo stats

--- backend/routes/stats.ts
+++ backend/routes/stats.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { PrismaClient} from "@prisma/client";
+import { MaintenanceStatus, PrismaClient} from "@prisma/client";
 import authenticateJWT from "../middleware/authenticateJWT.js";
 import dayjs from "dayjs";
 
@@ -30,23 +30,23 @@ router.get("/total-mantenimientos", async (_req: Request, res: Response) => {
 });
 
 // Agrupados por estado
-// router.get("/estado-mantenimientos", async (_req: Request, res: Response) => {
-//   try {
-//     const grouped = await prisma.maintenance.groupBy({
-//       by: ["status"],
-//       _count: { status: true },
-//     });
+router.get("/estado-mantenimientos", async (_req: Request, res: Response) => {
+  try {
+    const grouped = await prisma.maintenance.groupBy({
+      by: ["status"],
+      _count: { status: true },
+    });
 
-//     const result = grouped.map((g: { status: MaintenanceStatus; _count: { status: number } }) => ({
-//       status: g.status,
-//       count: g._count.status,
-//     }));
+    const result = grouped.map((g: { status: MaintenanceStatus; _count: { status: number } }) => ({
+      status: g.status,
+      count: g._count.status,
+    }));
 
-//     res.json(result);
-//   } catch {
-//     res.status(500).json({ message: "Error interno al agrupar mantenimientos" });
-//   }
-// });
+    res.json(result);
+  } catch {
+    res.status(500).json({ message: "Error interno al agrupar mantenimientos" });
+  }
+});
 
 // Mantenimientos por mes (últimos 6)
 router.get("/mantenimientos-por-mes", async (_req: Request, res: Response) => {
